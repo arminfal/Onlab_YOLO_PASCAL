ee685ffe60ed29febc6a1ecacdce7d33e66509ff
arminfal <arminf@mensa.hu>
2024-04-23 21:17:49 +0200

    Added flask webserver+ template for displaying results from the webserver

--- predict.py
+++ predict.py
@@ -1,23 +1,40 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_from_directory
 from werkzeug.utils import secure_filename
 from ultralytics import YOLO
 from PIL import Image
 import os
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+RESULT_FOLDER = 'results'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['RESULT_FOLDER'] = RESULT_FOLDER
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
         filename = secure_filename(file.filename)
-        file.save(os.path.join('uploads', filename))
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         
-        model = YOLO(f'/runs/segment/train/weights/best.pt')
-        results = model(os.path.join('uploads', filename), stream=True)
+        model = YOLO(f'runs\\segment\\train\\weights\\best.pt')
+        results = model(os.path.join(app.config['UPLOAD_FOLDER'], filename), stream=True)
         
-        # return results to user
-        return str(results)
+        # Process each frame in the results
+        for result in results:
+            # Plot results image
+            im_bgr = result.plot()  # BGR-order numpy array
+            im_rgb = Image.fromarray(im_bgr[..., ::-1])  # RGB-order PIL image
+            
+            # Create the directory if it doesn't exist
+            os.makedirs(app.config['RESULT_FOLDER'], exist_ok=True)
+            
+            # Save results to disk
+            result_filename = f"{filename}_result.jpg"
+            im_rgb.save(os.path.join(app.config['RESULT_FOLDER'], result_filename))
+            
+            # return results to user
+            return render_template('display.html', original=filename, result=result_filename)
     
     return '''
     <!doctype html>
@@ -29,5 +46,13 @@ def upload_file():
     </form>
     '''
 
+@app.route('/uploads/<filename>')
+def send_uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/results/<filename>')
+def send_result_file(filename):
+    return send_from_directory(app.config['RESULT_FOLDER'], filename)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8081)
